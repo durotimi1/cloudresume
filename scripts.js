fdb5ff872c99ce84d3d15f8c39373ceb7afe0de0
durotimi <durotimimusic@gmail.com>
2022-11-23 17:22:32 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,46 @@
+/*!
+* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+    const countEl =
+    document.getElementById('count');
+    updateVisitCount();
+    function updateVisitCount() {
+      fetch('api.countapi.xyz/update/florin-pop/youtube/?amount=1')
+      .then(res => res.json())
+      .then(res => {
+      countEl.innerHTML = res.value;
+      });
+
+   }
+});
+
+                       
